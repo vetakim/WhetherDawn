8e507091f3cd7d6734f1e32c25c630b1c024599d
Ким Виолетта <vkim@rti-mints.ru>
2017-04-23 02:08:30 +0300

    ome math errors were corrected. It can be a mistake in wiki, so the julian transition is calculated not completely as there.

--- WhetherDawn/WhetherDawnUp/astronomy.py
+++ WhetherDawn/WhetherDawnUp/astronomy.py
@@ -11,6 +11,10 @@ SECONDS = 3600
 PERIHELION = 102.9372
 MIDNIGHT = 2451545.5
 
+def juliantime2unix(jd):
+    '''converting julian date to unix time'''
+    return ( jd - 2440587.5 ) * 86400
+
 def dms2rads(d, m, s):
     '''degrees, minutes, seconds to radians'''
     degs = d + m / MINUTES + s / SECONDS
@@ -18,23 +22,22 @@ def dms2rads(d, m, s):
 
 def getJDN(_date):
     '''get julian day by grigorian date'''
-    a = trunc(( 14 - _date.month ) / 12)
+    a = ( 14 - _date.month ) // 12
     # years since March 1 -4800
     y = _date.year + JULIANYEAR - a
     # months since March 1 -4800
     m = _date.month + MONTHES * a - MARCH
-    jdn = _date.day + (153 * m + 2) / 5 + y * 365.2425 - 32045
-    return trunc(jdn)
+    jdn = _date.day + (153 * m + 2) // 5 + 365 * y + y // 4 - y // 100 + y // 400 - 32045
+    return jdn
 
-def getSunHourAngle(phi, _lambda, H):
+def getSunHourAngle(B, EL, H):
     '''getting the sun hour angle'''
     # altitude of the center of the solar disc
-    delta = asin(sin(_lambda) * sin(radians(23.44)))
-    a = -radians(0.83)
+    delta = asin(sin(EL) * sin(radians(23.44)))
+    # a = radians(-0.83 - 2.076 * sqrt(H) / 60)
+    a = radians(-0.83)
     # hour angle with refraction
-    w = acos((sin(a) - sin(phi) * sin(delta)) / cos(phi) / cos(delta))
-    # hour angle with observer's elevation correction
-    w += radians(-2.076) * sqrt(H) / radians(60)
+    w = acos((sin(a) - sin(B) * sin(delta)) / cos(B) / cos(delta))
     return w
 
 def getJulianTransit(_date, L):
@@ -42,14 +45,17 @@ def getJulianTransit(_date, L):
     # mean solar noon
     J = getJDN(_date) - degrees(L) / GRADUSES
     # solar mean anomaly
-    M = ((357.5291 + 0.98560028 * J) % GRADUSES)
+    M = (357.5291 + 0.98560028 * J) % GRADUSES
     # equation of the center
+    M = degrees(M)
     C = 1.9148 * sin(M) + 0.0200 * sin(2 * M) + 0.0003 * sin(3 * M)
     # ecliptic longitude
-    la = (( M + C + HALFGRADUSES + PERIHELION ) % GRADUSES)
+    M, C = degrees(M), degrees(C)
+    la = ( M + C + HALFGRADUSES + PERIHELION ) % GRADUSES
     # solar transit
-    Jtr = MIDNIGHT + J + 0.0053 * sin(M) - 0.0069 * sin(2 * la)
-    return Jtr
+    M, C, la = radians(M), radians(C), radians(la)
+    Jtr = J + 0.0053 * sin(M) - 0.0069 * sin(2 * la)
+    return Jtr, la
 
 def getDawnTime(B, L, H, _date):
     '''
@@ -60,8 +66,10 @@ def getDawnTime(B, L, H, _date):
     @param L - point longitude
     @param H - point height
     '''
-    dawn = getJulianTransit(_date, L) - getSunHourAngle(B, L, H)
-    return datetime.fromtimestamp( ( dawn - 2440587.5 ) * 86400)
+    jtr, el = getJulianTransit(_date, L)
+    w = getSunHourAngle(B, el, H)
+    dawn = jtr - degrees(w) / GRADUSES
+    return datetime.fromtimestamp(juliantime2unix(dawn))
 
 def getNightfallTime(B, L, H, _date):
     '''
@@ -72,6 +80,9 @@ def getNightfallTime(B, L, H, _date):
     @param L - point longitude
     @param H - point height
     '''
-    fall = getJulianTransit(_date, -L) + getSunHourAngle(B, L, H)
-    return datetime.fromtimestamp( ( fall - 2440587.5 ) * 86400)
+    jtr, el = getJulianTransit(_date, L)
+    w = getSunHourAngle(B, el, H)
+    fall = jtr + degrees(w) / GRADUSES
+    return datetime.fromtimestamp(juliantime2unix(fall))
+
 
