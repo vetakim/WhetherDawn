894bdfee512850834aeb18318273179d69ef6f9e
SShabanov <shabanovsb@gmail.com>
2017-04-27 23:26:20 +0300

    add model city

--- WhetherDawn/WhetherDawn/urls.py
+++ WhetherDawn/WhetherDawn/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'', include('WhetherDawnUp.urls')),
- #   url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
             # ... the rest of your URLconf goes here ...
             ] + static(settings.STATIC_URL,
                     document_root=settings.STATIC_ROOT)
